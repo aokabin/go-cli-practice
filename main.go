92c3a10fb012a1ea6cd09abb2d312edc99b39390
aokabin <info.aokabin@gmail.com>
2019-04-08 21:03:22 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "sampleApp"
+	app.Usage = "This app echo input arguments"
+	app.Version = "0.0.1"
+
+	app.Action = func(context *cli.Context) error {
+		if context.Bool("cat") {
+			fmt.Println(context.Args().Get(0) + "だにゃん♡")
+		} else {
+			fmt.Println(context.Args().Get(0))
+		}
+		return nil
+	}
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "cat, c",
+			Usage: "Echo with cat",
+		},
+	}
+
+	app.Run(os.Args)
+}
